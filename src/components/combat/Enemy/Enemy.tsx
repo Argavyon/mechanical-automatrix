6ae85215ce6f157920bbd8962261144d48936561
Tyler Jordan <1997.tylerjordan@gmail.com>
2022-04-23 16:41:18 +0300

    Added enemy health bar

--- src/components/combat/Enemy/Enemy.tsx
+++ src/components/combat/Enemy/Enemy.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import EnemyAttackBar from "./EnemyAttackBar";
 import "styles/combat/Enemy/Enemy.css";
+import EnemyHealth from "./EnemyHealth";
 
 type EnemyProps = { name: string };
 
@@ -8,6 +9,7 @@ const Enemy: React.FC<EnemyProps> = (props: EnemyProps) => {
   return (
     <div className="enemyContainer">
       <p className="text">{props.name}</p>
+      <EnemyHealth />
       <EnemyAttackBar attackTime={3} attackDamage={3} />
     </div>
   );
---
+++ src/components/combat/Enemy/EnemyHealth.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "styles/combat/Enemy/EnemyHealth.css";
+
+const EnemyHealth: React.FC = () => {
+  return (
+    <div className="enemyHealthContainer">
+      <p className="innerHealthBar">HealthBar</p>
+    </div>
+  );
+};
+
+export default EnemyHealth;
